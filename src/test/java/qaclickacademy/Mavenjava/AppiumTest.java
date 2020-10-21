889a5ba8eec49b129c636c5debd0f8964c1fdeef
user <sagari2103@gmail.com>
2020-10-20 22:17:18 -0700

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void Androidapp() {
+		System.out.println("Androidapps");
+		
+	}
+	
+	@Test
+	public void IOSapp() {
+		System.out.println("IOSapp");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void PostJIRA() {
+		System.out.println("PostJIRA");
+		
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
